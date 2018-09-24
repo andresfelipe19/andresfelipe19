3363462fb65e2f1da89a367a4a1425ecf93f40b3
andres <andresfelipe@gmail.com>
2018-09-24 16:13:27 -0500

    pruebas con variable javascript y estructura basica

---
+++ scrytps/app.js
@@ -0,0 +1,36 @@
+/*
+var numero1 = 10;
+var numero2 = 5;
+var resultado = numero1*numero2;
+
+console.log(resultado);
+
+var nombre = "andres ballesteros";
+var profesion = "futbolista millonarios fc";
+
+console.log(nombre + profesion);
+}
+
+ var tienen_mascota = true;
+
+console.log(tienen_mascota);
+
+ var datos=["andres",19,falso,25,78,"Desarrollador"];
+
+ console.log(datos[0]);
+*/
+
+var edad = 25;
+ if(edad>17 && edad<=40){
+ 	console.log("eres un adulto joven");
+ }else{
+ console.log("no eres un adulto joven");
+
+}
+
+var dias= ['lunes','martes','miercoles','jueves','sabado'];
+
+for(var a=0;a<7;a++){
+	console.log(dias[a]);
+
+}
